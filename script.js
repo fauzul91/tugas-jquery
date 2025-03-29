e37f76ae5062e6e2a3c6798403afca83005891e7
Fauzul Akbar <fauzulakbar2575@gmail.com>
2025-03-29 16:01:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $("#successModal").hide(); 
+    $("#btn-login").click(function (e) {
+        e.preventDefault();
+
+        let isValid = true;
+        
+        let firstName = $("#firstName").val().trim();
+        let lastName = $("#lastName").val().trim();
+        let email = $("#email").val().trim();
+        let password = $("#password").val().trim();
+        let confirmPassword = $("#confirmPassword").val().trim();
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        $(".error").text("");
+
+        if (firstName === "") {
+            $("#errorFirstName").text("First Name tidak boleh kosong!");
+            isValid = false;
+        }
+        else if (lastName === "") {
+            $("#errorLastName").text("Last Name tidak boleh kosong!");
+            isValid = false;
+        }
+        else if (email === "" || !emailPattern.test(email)) {
+            $("#errorEmail").text("Email tidak valid!");
+            isValid = false;
+        }
+        else if (password === "") {
+            $("#errorPassword").text("Password tidak boleh kosong!");
+            isValid = false;
+        }
+        else if (confirmPassword === "") {
+            $("#errorConfirmPassword").text("Konfirmasi Password harus diisi!");
+            isValid = false;
+        }
+        else if (password !== confirmPassword) {
+            $("#errorConfirmPassword").text("Password tidak cocok!");
+            isValid = false;
+        }
+
+        if (isValid) {
+            $("#successModal").fadeIn();
+        }
+    });
+
+    $("#closeModal").click(function () {
+        $("#successModal").fadeOut();
+    });
+});
